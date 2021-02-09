78c72db5016fb3faccde95099dcc82c6cb1c4528
Lyle-Rogers <lylerogers8@gmail.com>
2021-02-09 16:01:17 -0700

    More stripe testing

--- config/stripe.js
+++ config/stripe.js
@@ -2,8 +2,8 @@
 'use strict'
 
 module.exports = {
-  key_publishable: process.env.STRIPE_PUBLIC_KEY,
-  key_secret: process.env.STRIPE_SECRET_KEY,
+  key_publishable: 'pk_test_51IF0rZGmBT1pM9j5DkeplMrDcGutoFNi7y8kW4x89gRewqUB4vJ7SwV7Ed9f4TXyYqn9RPIj1jSD62KwZeHl88iV00DitogTfd',
+  key_secret: 'sk_test_51IF0rZGmBT1pM9j5Z6zNjvTKhv7qD4G1rnESqtJ7sAofgsxHkBXQmqaJqibLnFRIYarVOGkWv2IRcFjKwyueOxwX001xoRAUhd',
   url_success: process.env.APP_URL + "/pay/success",
   url_error: process.env.APP_URL + "/pay/error"
 }
