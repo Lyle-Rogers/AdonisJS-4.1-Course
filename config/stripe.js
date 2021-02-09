53f7fb4acdae8b2f722099348efc1ce34f31d5e9
Lyle-Rogers <lylerogers8@gmail.com>
2021-02-09 15:46:36 -0700

    I feel like a programming genius!

--- config/stripe.js
+++ config/stripe.js
@@ -2,8 +2,8 @@
 'use strict'
 
 module.exports = {
-  key_publishable: process.env.get('STRIPE_PUBLIC_KEY'),
-  key_secret: process.env.get('STRIPE_SECRET_KEY'),
-  url_success: process.env.get('APP_URL') + "/pay/success",
-  url_error: process.env.get('APP_URL') + "/pay/error"
+  key_publishable: process.env.STRIPE_PUBLIC_KEY,
+  key_secret: process.env.STRIPE_SECRET_KEY,
+  url_success: process.env.APP_URL + "/pay/success",
+  url_error: process.env.APP_URL + "/pay/error"
 }
