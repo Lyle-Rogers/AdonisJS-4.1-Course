a9ae71a292fa7cdce0a34461872dd81d072bd052
Lyle-Rogers <lylerogers8@gmail.com>
2021-02-09 15:29:48 -0700

    Testing payments

--- config/stripe.js
+++ config/stripe.js
@@ -1,11 +1,9 @@
 //config/stripe.js
 'use strict'
 
-const Env = use('Env')
-
 module.exports = {
-  key_publishable: Env.get('STRIPE_PUBLIC_KEY'),
-  key_secret: Env.get('STRIPE_SECRET_KEY'),
-  url_success: Env.get('APP_URL') + "/pay/success",
-  url_error: Env.get('APP_URL') + "/pay/error"
+  key_publishable: process.env.get('STRIPE_PUBLIC_KEY'),
+  key_secret: process.env.get('STRIPE_SECRET_KEY'),
+  url_success: process.env.get('APP_URL') + "/pay/success",
+  url_error: process.env.get('APP_URL') + "/pay/error"
 }
